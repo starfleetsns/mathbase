ad71df072088cdda54567284ea24ee5b91f36c74
trenta3 <dario.balboni.96@gmail.com>
2016-03-18 22:35:53 +0100

    Django app going in production

--- mathbase/settings.py
+++ mathbase/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mathbase.herokuapp.com']
 
 
 # Application definition
@@ -57,6 +57,7 @@ LOGIN_REDIRECT_URL = '/tree/'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
     'default': {
@@ -64,6 +65,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'] = dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -83,3 +85,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+)
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
